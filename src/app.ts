76f437a7db1bd18380a4331ebe038a455d48ebb8
felipeLeao18 <flpleao1807@gmail.com>
2022-11-03 22:20:35 -0300

    feat: add express middleware to error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import { router } from './routes'
 import cors from 'cors'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(router)
+
+app.use((err: { err: Error, statusCode: number, message: string }, _req: Request, res: Response, _next: NextFunction) => {
+  if (err.err && err.err instanceof Error) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+      status: 'error'
+    })
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 export { app }
